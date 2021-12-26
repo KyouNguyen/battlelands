7b3a34ec9313e4e1e3fe84d64d4b1b4e7f78f3f3
Nguyen Agile <agile.wp@agiletech.vn>
2021-12-26 23:03:05 +0700

    update 26/12/2021

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,24 @@ jQuery(document).ready(function($) {
 
     
 });
+
+jQuery(document).ready(function($) {
+
+    $(document).on('data-char-changed', function() {
+        var index = $('.characters_weapons_right .img_box');
+        setTimeout(function() {
+            index.delay(260).attr('data-aos', 'fade-up');
+        }, 260);
+        setTimeout(function() {
+            index.delay(500).attr('data-aos', 'fade-up').children('img').attr('src', index.data('src'));
+            index.addClass('aos-init aos-animate');
+        }, 450);
+    });
+    $('.item_charactor').on('click', function() {
+        var index = $('.characters_weapons_right .img_box');
+        index.attr('data-aos', 'fade-down');
+        index.removeClass('aos-init aos-animate');
+        index.data('src', $(this).data('img'));
+        $(document).trigger('data-char-changed');
+    });
+});
